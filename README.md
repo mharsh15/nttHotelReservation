ed479c2c89f16e3c31159394c2390442f95d4ca9
mharsh15 <mharsh15@gmail.com>
2022-10-16 16:04:33 +0530

    update readme file for routes. update seed file for adding 7 rows of data

---
+++ README.md
@@ -0,0 +1,31 @@
+# Reservation API
+
+## Routes
+- /summary 
+    (GET)
+    : Displays summary for all users like total cancelled bookings, total bookings
+- /reservation 
+    (GET)
+    : Shows all upcoming reservations
+- /reservation/id
+    (GET)
+    :shows all reservations
+- /reservation/:id
+    (GET)
+    :shows reservation for particular ID
+- /reservation
+    (POST)
+    :adds an reservation to a particular guest and updates summary database
+- /reservation/:id
+    (POST)
+    :updates reservation status to false and updates summary database          
+
+
+### IDS for summary for testing
+- Test Reservation: 634bda2e7554f49500745a11
+- 634bda2e7554f49500745a10 - Magdanela
+- 634bda2e7554f49500745a0f - Gloria
+- 634bda2e7554f49500745a0e - Jessica
+- 634bda2e7554f49500745a0d - Olaf
+- 634bda2e7554f49500745a0c - Jacob
+- 634bda2e7554f49500745a0b - Zester
--- seeds/summarySeeds.js
+++ seeds/summarySeeds.js
@@ -45,6 +45,83 @@ async function insertData(){
                 cancelled_stays:0,
                 total_stays:3
             },
+            {
+                name:"Olaf Schultz",
+                upcoming_stays:{
+                    total_stays:0,
+                    total_nights:0,
+                    total_amount:0
+                },
+                past_stays:{
+                    total_stays:10,
+                    total_nights:22,
+                    total_amount:86000
+                },
+                cancelled_stays:5,
+                total_stays:10
+            },
+        
+            {
+                name:"Jessica Arden",
+                upcoming_stays:{
+                    total_stays:5,
+                    total_nights:20,
+                    total_amount:89000
+                },
+                past_stays:{
+                    total_stays:0,
+                    total_nights:0,
+                    total_amount:0
+                },
+                cancelled_stays:0,
+                total_stays:5
+            },
+            {
+                name:"Gioria Meloni",
+                upcoming_stays:{
+                    total_stays:1,
+                    total_nights:2,
+                    total_amount:1800
+                },
+                past_stays:{
+                    total_stays:0,
+                    total_nights:0,
+                    total_amount:0
+                },
+                cancelled_stays:1,
+                total_stays:1
+            },
+        
+            {
+                name:"Magdalena Andersson",
+                upcoming_stays:{
+                    total_stays:1,
+                    total_nights:10,
+                    total_amount:400000
+                },
+                past_stays:{
+                    total_stays:3,
+                    total_nights:12,
+                    total_amount:12000000
+                },
+                cancelled_stays:5,
+                total_stays:4
+            },
+            {
+                name:"NTT TEST",
+                upcoming_stays:{
+                    total_stays:0,
+                    total_nights:0,
+                    total_amount:0
+                },
+                past_stays:{
+                    total_stays:0,
+                    total_nights:0,
+                    total_amount:0
+                },
+                cancelled_stays:0,
+                total_stays:0
+            },
         ]
         
         
